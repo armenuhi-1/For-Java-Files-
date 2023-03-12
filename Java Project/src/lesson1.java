8b93f189ac66c7e846e84403b076841f7159ac20
Armneuhi Khachatryan <armenuhi.khachatryan2000@gmail.com>
2023-03-12 21:26:41 +0400

    My first git commit

---
+++ Java Project/src/lesson1.java
@@ -0,0 +1,37 @@
+public class lesson1 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Lesson 1");
+
+        byte u = 16;
+        System.out.println(u);
+
+        short x = 3000;
+        System.out.println(x);
+
+        float aa = 12.34f;
+        System.out.println(aa);
+        System.out.println(aa);
+
+        double xx = 1234.32;
+        System.out.println(xx);
+
+
+        boolean w = false;
+        w = true;
+        System.out.println(w);
+
+        char yy = '*';
+        System.out.println(yy);
+
+
+        String placeName = "Sevan";
+        String userName = "aaa";
+        System.out.println(placeName + " " + userName);
+
+        int h = 65;
+        System.out.println("h = " + h);
+
+
+    }
+}
