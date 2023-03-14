478b9d00eeb4e31f352150c32db316e7513928a1
Armneuhi Khachatryan <armenuhi.khachatryan2000@gmail.com>
2023-03-14 16:16:41 +0400

    My first Java files

---
+++ Operations/src/Lesson2.java
@@ -0,0 +1,36 @@
+public class Lesson2 {
+    public static void main(String[] args) {
+        int x;
+        int _x = 9;
+        System.out.println(_x);
+        int a45 = 92;
+        System.out.println(a45);
+
+        String firstName = "Jack";
+        System.out.println(firstName);
+
+        int a = 4, b = 3;
+        int c = a + b;
+        System.out.println(c);
+
+        double t = 7.06, p = 5.0;
+        System.out.println(t / p);
+
+        x = 4;
+        System.out.println(x++);
+        System.out.println(x);
+
+        x = 41;
+        System.out.println(x % 2 == 0);
+
+        System.out.println(x >= 1 && x <= 9);
+        boolean ar = x >= 1 && x <= 9;
+
+        boolean ww = x < 0 || x > 100;
+
+        ww = true;
+        System.out.println(ww);
+
+
+    }
+}
